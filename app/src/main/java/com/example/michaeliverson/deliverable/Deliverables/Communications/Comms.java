e1f0aba607deff80fa7994732232cba18d38f2fa
Mikypi3.14159 <Mikypi3.14159@gmail.com>
2018-04-11 06:14:51 -0400

    Morning Commit 2

--- app/src/main/java/com/example/michaeliverson/deliverable/Deliverables/Communications/Comms.java
+++ app/src/main/java/com/example/michaeliverson/deliverable/Deliverables/Communications/Comms.java
@@ -1,10 +1,38 @@
 package com.example.michaeliverson.deliverable.Deliverables.Communications;
 
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.moshi.MoshiConverterFactory;
+
 /**
  * Created by michaeliverson on 4/10/18.
  */
 
 public class Comms {
 
+
+    // Retrofit implmentation
+    private Retrofit retrofit;
+
+    public  Comms()
+    {
+    }
+
+    public void builder()
+    {
+        RxJava2CallAdapterFactory rxAdapter = RxJava2CallAdapterFactory.create();
+        this.retrofit = new Retrofit.Builder()
+                        .baseUrl("https://maps.googleapis.com")
+                        .addConverterFactory(MoshiConverterFactory.create())
+                        .addCallAdapterFactory(rxAdapter)
+                        .build();
+    }
+
+
+    public void grabData(String lat,String lng,String desination,String apiKey)
+    {
+
+    }
 
 }
--- app/src/main/java/com/example/michaeliverson/deliverable/Deliverables/Communications/Contract.java
+++ app/src/main/java/com/example/michaeliverson/deliverable/Deliverables/Communications/Contract.java
@@ -1,9 +1,12 @@
 package com.example.michaeliverson.deliverable.Deliverables.Communications;
 
-import com.example.michaeliverson.deliverable.Deliverables.Convertion.CartesianClass;
+import com.example.michaeliverson.deliverable.Data.Route;
+
+import java.util.ArrayList;
 
 import io.reactivex.Observable;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 /**
  * Created by michaeliverson on 4/10/18.
@@ -11,7 +14,6 @@ import retrofit2.http.GET;
 
 public interface Contract {
 
-    @GET("/maps/api/directions/")
-    Observable<ArrayList<Route>>Direction()
-
+    @GET("/maps/api/directions/json?")
+    Observable<ArrayList<Route>>Direction(@Query("origin")double lat, double lng, @Query("desination") String destination, @Query("key") String key);
 }
